f3197a377125fc63856cfb63c557d1ec1169262f
a1phaZ <pride.ots@gmail.com>
2021-06-25 14:51:00 +0300

    feat: styling

--- src/styledComponents/Inputs.styled-components.js
+++ src/styledComponents/Inputs.styled-components.js
@@ -1,50 +1,79 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  font-size: 20px;
+  font-size: 1rem;
   font-weight: 200;
   letter-spacing: 1px;
   padding: 13px 50px 13px;
   outline: 0;
-  border: 1px solid black;
+  border: 1px solid #cacaca;
   cursor: pointer;
   position: relative;
   background-color: rgba(0, 0, 0, 0);
+  width: 100%;
 `
 
 export const Input = styled.input`
-  flex: 1 1 100%;
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
+  font-size: 20px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  padding: 13px;
+  outline: 0;
+  border: 1px solid #cacaca;
+  cursor: text;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0);
 `
 
 export const LinkButton = styled.button`
   background: none!important;
   border: none;
   padding: 0!important;
-  /*optional*/
   font-family: arial, sans-serif;
-  /*input has OS specific font-family*/
   color: #069;
   text-decoration: underline;
   cursor: pointer;
 `
 
+export const SearchInput = styled(Input)`
+  flex-grow: 1;
+  font-size: 1rem;
+  font-weight: 200;
+  margin-bottom: 1rem;
+  @media(min-width: 768px) {
+    margin-bottom: 0;
+    margin-right: 1rem;
+  }
+`
+
+export const FormInput = styled(Input)`
+  flex: 1 1 100%;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 0.5rem 0 1rem 0;
+  box-sizing: border-box;
+`
+
 export const OpenModalButton = styled(Button)`
-  margin: 8px 0;
+  background-color:#17a2b8;
+  color: #fff;
+  @media(min-width: 768px) {
+    width: auto;
+  }
 `
 
 export const AddButton = styled(Button)`
-    
+  background-color: #28a745;
+  color: #fff;
 `
 
 export const CloseModalButton = styled.span`
   color: #aaa;
   float: right;
-  font-size: 28px;
+  font-size: 1rem;
+  line-height: 1rem;
   font-weight: bold;
+  cursor: pointer;
   &:hover: {
     color: black;
     text-decoration: none;
