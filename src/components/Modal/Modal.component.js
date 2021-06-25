af8740b1fa753ab3bc062527ed9280388ddd4c2c
a1phaZ <pride.ots@gmail.com>
2021-06-25 10:39:34 +0300

    feat: add item to localstorage, clear form, close modal

--- src/components/Modal/Modal.component.js
+++ src/components/Modal/Modal.component.js
@@ -3,45 +3,63 @@ import {Form, FormField, Modal, ModalContent} from "../../styledComponents/Conta
 import {AddButton, CloseModalButton, Input, InputLabel} from "../../styledComponents/Inputs.styled-components";
 
 const initialState = {
-  formData: {
-    title: '',
-    url: ''
-  }
+  title: '',
+  url: '',
+  category: ''
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FIELD_CHANGE': {
-      const {formData} = state;
-      formData[action.payload.field] = action.payload.value;
       return {
         ...state,
-        formData
+        [action.payload.field]: action.payload.value
       }
     }
-    default: return state;
+    case 'CLEAR': {
+      return {
+        title: '',
+        url: '',
+        category: ''
+      }
+    }
+    default:
+      return state;
   }
 }
 
-const ModalPage = ({showModal, onModalToggle}) => {
+const ModalPage = ({showModal, setShowModal, addLink}) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   function onFieldChange(e) {
     dispatch({type: 'FIELD_CHANGE', payload: {field: e.currentTarget.id, value: e.currentTarget.value}});
   }
 
+  function clearForm() {
+    dispatch({type: 'CLEAR'});
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    addLink(state);
+    clearForm();
+    setShowModal(false);
+  }
+
   return (
     <Modal showModal={showModal} id={'add-modal'}>
       <ModalContent className={'add-modal-content'}>
-        <CloseModalButton onClick={onModalToggle()}>X</CloseModalButton>
+        <CloseModalButton onClick={() => setShowModal(false)}>X</CloseModalButton>
         <Form>
           <FormField>
             <InputLabel htmlFor={'title'}>Название</InputLabel>
-            <Input id={'title'} placeholder={'Название'} value={state.title} onChange={onFieldChange}/>
-            <InputLabel htmlFor={'url'}>Адрес</InputLabel>
-            <Input id={'url'} placeholder={'Адрес'} value={state.url} onChange={onFieldChange}/>
-            <AddButton>Добавить ссылку</AddButton>
+            <Input id={'title'} placeholder={'Название'} value={state.title} onChange={onFieldChange} required/>
           </FormField>
+          <FormField>
+            <InputLabel htmlFor={'url'}>Адрес</InputLabel>
+            <Input id={'url'} placeholder={'Адрес'} value={state.url} onChange={onFieldChange} required/>
+          </FormField>
+          <AddButton type={'button'} onClick={onSubmit}>Добавить ссылку</AddButton>
         </Form>
 
       </ModalContent>
