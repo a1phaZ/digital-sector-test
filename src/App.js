828f103911c39196edbf055e24271756cb270a0b
a1phaZ <pride.ots@gmail.com>
2021-06-25 14:54:32 +0300

    feat: add link && adding mock data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useLayoutEffect} from 'react';
+import React, {useEffect, useState, useLayoutEffect, useCallback} from 'react';
 import useLocalStorage from "./hooks/useLocalStorage";
 import {Container, Content, HeaderContainer, Sidebar} from "./styledComponents/Containers.styled-components";
 import LinksList from "./components/LinksList/LinksList.component";
@@ -15,12 +15,20 @@ const mock = [{
 }, {
   title: 'ReactJS',
   url: 'https://ru.reactjs.org/',
-  category: 'js'
+  category: 'js framework'
 }, {
   title: 'Artemiy Zebzeev',
   url: 'https://github.com/a1phaZ',
-  category: 'dev'
-}];
+  category: 'developer'
+}, {
+  title: 'Vue.js',
+  url: 'https://vuejs.org/',
+  category: 'js framework'
+}, {
+  title: 'Google',
+  url: 'https://www.google.com/',
+  category: 'search'
+},];
 
 function App() {
   const [links, setLinks] = useLocalStorage('links');
@@ -48,31 +56,32 @@ function App() {
     setSearch(e.currentTarget.value);
   }
 
-  function onModalToggle() {
+  const onModalToggle = useCallback(() => {
     return () => {
       if (!showModal) window.addEventListener('click', closeModalByWindow());
       if (showModal) window.removeEventListener('click', closeModalByWindow());
       setShowModal(!showModal);
     }
-  }
+  }, [showModal]);
 
   function closeModalByWindow() {
     return (e) => {
       const modal = document.getElementById('add-modal');
       if (e.target === modal) setShowModal(false);
     }
+  }
 
+  function addLink(formData) {
+    setLinks([...links, formData]);
   }
 
   return (
     <Container className="App">
-      <ModalPage showModal={showModal} onModalToggle={onModalToggle}/>
-      <Container justifyContent={'flex-end'}>
+      <ModalPage showModal={showModal} setShowModal={setShowModal} addLink={addLink}/>
+      <HeaderContainer className={'header'}>
         <SearchForm onSearch={onSearch}/>
-        <HeaderContainer>
-          <OpenModalButton onClick={onModalToggle()}>Добавить ссылку</OpenModalButton>
-        </HeaderContainer>
-      </Container>
+        <OpenModalButton onClick={onModalToggle()}>Добавить ссылку</OpenModalButton>
+      </HeaderContainer>
       <Sidebar className={'filter-bar'}>
         <FilterBar data={links} onFilter={onFilterClick}/>
       </Sidebar>
